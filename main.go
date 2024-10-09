0965c56bd3dfa5733cb4785b7e04d6abd69c8a55
Михаил Те <tpl20000@yandex.ru>
2024-10-10 02:52:50 +0700

    Fix variable namings
    Snakecase to camelcase

--- main.go
+++ main.go
@@ -14,21 +14,21 @@ func main() {
 	//send get request to the host on srv.msk01.gigacorp.local
 	//and print the response
 
-	error_count := 0
+	errorCount := 0
 
 	for {
 		time.Sleep(2 * time.Second)
 
-		if error_count >= 3 {
+		if errorCount >= 3 {
 			fmt.Println("Unable to fetch server statistic")
-			error_count = 0
+			errorCount = 0
 		}
 
 		resp, err := http.Get("http://srv.msk01.gigacorp.local")
 
 		//check if http request was successful
 		if err != nil {
-			error_count++
+			errorCount++
 			continue
 		}
 
@@ -40,14 +40,14 @@ func main() {
 
 			if content_type != "text/plain; charset=utf-8" {
 				fmt.Println("bad content type, received:", content_type)
-				error_count++
+				errorCount++
 				continue
 			}
 
 		} else {
 			//http responce status code is not ok (not 200)
 			fmt.Println("http responce status not 200")
-			error_count++
+			errorCount++
 			continue
 		}
 
@@ -56,7 +56,7 @@ func main() {
 		resp.Body.Close()
 		if err != nil {
 			fmt.Println("unable to read response body")
-			error_count++
+			errorCount++
 			continue
 		}
 
@@ -66,62 +66,62 @@ func main() {
 		//check if values are ok
 		if len(values) != 7 {
 			fmt.Println("wrong amount of values")
-			error_count++
+			errorCount++
 			continue
 		}
 
-		average_load, err := strconv.Atoi(values[0])
+		averageLoad, err := strconv.Atoi(values[0])
 		if err != nil {
 			continue
 		}
 
-		max_ram, err := strconv.Atoi(values[1])
+		maxRam, err := strconv.Atoi(values[1])
 		if err != nil {
 			continue
 		}
 
-		used_ram, err := strconv.Atoi(values[2])
+		usedRam, err := strconv.Atoi(values[2])
 		if err != nil {
 			continue
 		}
 
-		disk_space_bytes, err := strconv.Atoi(values[3])
+		diskSpaceBytes, err := strconv.Atoi(values[3])
 		if err != nil {
 			continue
 		}
 
-		used_disk_space_bytes, err := strconv.Atoi(values[4])
+		usedDiskSpaceBytes, err := strconv.Atoi(values[4])
 		if err != nil {
 			continue
 		}
 
-		net_throughput_bytes_per_s, err := strconv.Atoi(values[5])
+		netThroughputBytesPerS, err := strconv.Atoi(values[5])
 		if err != nil {
 			continue
 		}
 
-		net_load_bytes_per_s, err := strconv.Atoi(values[6])
+		netLoadBytesPerS, err := strconv.Atoi(values[6])
 		if err != nil {
 			continue
 		}
 
-		if average_load > 30 {
-			fmt.Println("Load Average is too high:", average_load)
+		if averageLoad > 30 {
+			fmt.Println("Load Average is too high:", averageLoad)
 		}
 
-		if float64(used_ram)/float64(max_ram) > 0.8 {
-			ram_usage_percent_str := strconv.FormatFloat(float64(used_ram)/float64(max_ram)*100, 'f', 0, 64) + "%"
-			fmt.Println("Memory usage too high:", ram_usage_percent_str)
+		if float64(usedRam)/float64(maxRam) > 0.8 {
+			ramUsagePercentStr := strconv.FormatFloat(float64(usedRam)/float64(maxRam)*100, 'f', 0, 64) + "%"
+			fmt.Println("Memory usage too high:", ramUsagePercentStr)
 		}
 
-		if float64(used_disk_space_bytes)/float64(disk_space_bytes) > 0.9 {
-			disk_usage_percent_str := strconv.FormatInt(int64((disk_space_bytes-used_disk_space_bytes)/1048576), 10)
-			fmt.Println("Free disk space is too low:", disk_usage_percent_str, "Mb left")
+		if float64(usedDiskSpaceBytes)/float64(diskSpaceBytes) > 0.9 {
+			diskUsagePercentStr := strconv.FormatInt(int64((diskSpaceBytes-usedDiskSpaceBytes)/1048576), 10)
+			fmt.Println("Free disk space is too low:", diskUsagePercentStr, "Mb left")
 		}
 
-		if float64(net_load_bytes_per_s)/float64(net_throughput_bytes_per_s) > 0.9 {
-			net_available_mbits_str := strconv.FormatInt(int64((net_throughput_bytes_per_s-net_load_bytes_per_s)/1000000), 10)
-			fmt.Println("Network bandwidth usage high:", net_available_mbits_str, "Mbit/s available")
+		if float64(netLoadBytesPerS)/float64(netThroughputBytesPerS) > 0.9 {
+			netAvailableMbitsStr := strconv.FormatInt(int64((netThroughputBytesPerS-netLoadBytesPerS)/1000000), 10)
+			fmt.Println("Network bandwidth usage high:", netAvailableMbitsStr, "Mbit/s available")
 		}
 
 	}
