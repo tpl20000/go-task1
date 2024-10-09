f53d96c7c13ebc4dd6d7462dcb4577a8cef194e4
Михаил Те <tpl20000@yandex.ru>
2024-10-10 01:06:06 +0700

    Logic fixes in main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -13,14 +14,21 @@ func main() {
 	//send get request to the host on srv.msk01.gigacorp.local
 	//and print the response
 
-	fetch_success := false
+	error_count := 0
 
-	for i := 0; i < 3; i++ {
+	for {
+		time.Sleep(2 * time.Second)
+
+		if error_count >= 3 {
+			fmt.Println("Unable to fetch server statistic")
+			error_count = 0
+		}
+
 		resp, err := http.Get("http://srv.msk01.gigacorp.local")
 
 		//check if http request was successful
 		if err != nil {
-			fmt.Println("Http receive error. Trying again...")
+			error_count++
 			continue
 		}
 
@@ -31,61 +39,72 @@ func main() {
 			content_type := resp.Header.Get("Content-Type")
 
 			if content_type != "text/plain; charset=UTF-8" {
+				error_count++
 				continue
 			}
 
 		} else {
 			//http responce status code is not ok (not 200)
+			error_count++
 			continue
 		}
 
 		//read responce body
 		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
 		if err != nil {
+			error_count++
 			continue
 		}
-		defer resp.Body.Close()
 
 		//split responce body by comma
 		values := strings.Split(string(body), ",")
 
 		//check if values are ok
 		if len(values) != 7 {
+			error_count++
 			continue
 		}
 
 		average_load, err := strconv.Atoi(values[0])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		max_ram, err := strconv.Atoi(values[1])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		used_ram, err := strconv.Atoi(values[2])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		disk_space_bytes, err := strconv.Atoi(values[3])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		used_disk_space_bytes, err := strconv.Atoi(values[4])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		net_throughput_bytes_per_s, err := strconv.Atoi(values[5])
 		if err != nil {
+			error_count++
 			continue
 		}
 
 		net_load_bytes_per_s, err := strconv.Atoi(values[6])
 		if err != nil {
+			error_count++
 			continue
 		}
 
@@ -99,7 +118,7 @@ func main() {
 		}
 
 		if float64(used_disk_space_bytes/disk_space_bytes) > 0.9 {
-			disk_usage_percent_str := strconv.FormatFloat(float64(used_disk_space_bytes/disk_space_bytes)*100, 'f', 2, 64)
+			disk_usage_percent_str := strconv.FormatInt(int64(used_disk_space_bytes-disk_space_bytes/1048576), 10)
 			fmt.Println("Free disk space is too low:", disk_usage_percent_str, "Mb left")
 		}
 
@@ -108,12 +127,5 @@ func main() {
 			fmt.Println("Network load is too high:", net_load_percent_str)
 		}
 
-		fetch_success = true
-		break
-
-	}
-
-	if !fetch_success {
-		fmt.Println("Unable to fetch server statistic")
 	}
 }
